b9961dd7466b03512758d65ac0fffb5c0b3811c9
Jenseko <jenscode23@gmail.com>
2023-04-27 14:44:12 +0200

    detailspage in progress

--- src/pages/GameDetailsPage/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage/GameDetailsPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import style from './GameDetailsPage.scss?inline'
+import style from './GameDetailsPage.module.scss'
+
+import { useState, useEffect } from 'react';
 
 const GameDetailsPage = () => {
 
@@ -9,17 +11,24 @@ const GameDetailsPage = () => {
 		fetch('https://www.freetogame.com/api/games')
 			.then(res => res.json())
 			.then(data => {
-				console.log(data)
 				setGameDetails(data)
-				console.log(gameDetails)
-
 			})
 	}, [])
 
+	console.log(gameDetails)
 
 	return (
 		<div className={style.gameDetailsPage}>
-			GameDetailsPage
+			<main>
+				<section>
+					<img src={gameDetails[0].thumbnail} alt={`thumbnail ${gameDetails[0].title}`} />
+				</section>
+				<h2>{gameDetails[0].title}</h2>
+				<article>
+					<div>
+					</div>
+				</article>
+			</main>
 		</div>
 	);
 };
