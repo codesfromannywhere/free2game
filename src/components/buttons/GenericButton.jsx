3a5a1efb52301cea13ec1989b0004c6d8d14073e
Justin Schildt <Just.schildt@gmail.com>
2023-04-27 16:20:07 +0200

    Added Generic Button Component

--- src/components/buttons/GenericButton.jsx
+++ src/components/buttons/GenericButton.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import style from './GenericButton.module.scss'
 
-const GenericButton = ({children}) => {
+const GenericButton = ({
+	children,
+	onClick,
+	className,
+	disabled,
+	...props
+}) => {
 	return (
-		<button className={style.genericButton}>
+		<button
+			className={className}
+			onClick={onClick ? onClick : undefined}
+			disabled={disabled}
+			{...props}
+		>
 			{children}
 		</button>
 	);
 };
 
 export default GenericButton;
+
+
