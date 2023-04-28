135573000df9b15a341342c235ad92dfae617e67
codesfromannywhere <codesfromannywhere@gmail.com>
2023-04-28 11:44:10 +0200

    added hero image

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,12 +4,13 @@ import HomeRecentList from '../../components/home/HomeRecentList'
 import TopGamesBrowserList from '../../components/home/TopGamesBrowserList'
 import TopGamesPCList from '../../components/home/TopGamesPCList'
 import HeroSection from '../../components/HeroSection'
+import image from '../../assets/images/logo.svg'
 
 
 const HomePage = () => {
 	return (
 		<main className={style.homePage}>
-			<HeroSection title="Find & track the best free-to-play games!" />
+			<HeroSection title="Find & track the best free-to-play games!" backgroundImage={image} />
 			<HomeRecentList />
 			<TopGamesPCList />
 			<TopGamesBrowserList />
