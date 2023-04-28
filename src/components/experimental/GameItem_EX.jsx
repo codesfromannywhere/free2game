857a5729d733d8212d6dfb5b93d062b6f0ad459a
Justin Schildt <Just.schildt@gmail.com>
2023-04-28 10:03:32 +0200

    Created Experimental GameItem

---
+++ src/components/experimental/GameItem_EX.jsx
@@ -0,0 +1,24 @@
+import windowsIcon from "../../assets/images/icon_windows.svg"
+import BrowserIcon from "../../assets/images/icon_browser.svg"
+import GenericLink from "../buttons/GenericLink.jsx";
+
+const GameItem_EX = (props) => {
+    const description = props.description ? (<p>{props.description}</p>) : "";
+
+    return (
+        <section>
+            <img src={props.img} alt={props.title} />
+            <article className="gameCard">
+                <h3>{props.title}</h3>
+                {description}
+                <GenericLink>Read More</GenericLink>
+            </article>
+            <article>
+                <img src={props.platform === "Web Browser" ? BrowserIcon : windowsIcon} alt="platform icon" />
+                <p>{props.genre}</p>
+            </article>
+        </section>
+    );
+}
+
+export default GameItem_EX;
