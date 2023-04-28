5f7fe5af9b99759683ad4fe6409c7fbcc233a738
Justin Schildt <Just.schildt@gmail.com>
2023-04-28 11:41:21 +0200

    Fixed background image url... again...

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = ({title, backgroundImage}) => {
 		<header
 			className={style.heroSection}
 			style={{
-				backgroundImage: url(backgroundImage)
+				backgroundImage: `url(${backgroundImage})`
 			}}
 		>
 			{
