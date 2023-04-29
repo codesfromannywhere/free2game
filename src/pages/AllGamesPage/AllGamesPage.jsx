59800a645b74ae665912112beac45fa26309b2cb
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-04-29 17:38:30 +0200

    Suchfunktion AllGamesPage hinzugefügt

--- src/pages/AllGamesPage/AllGamesPage.jsx
+++ src/pages/AllGamesPage/AllGamesPage.jsx
@@ -3,26 +3,47 @@ import style from './AllGamesPage.module.scss';
 import { SearchTextContext } from '../../components/layout/Layout.jsx';
 import { useState, useEffect, useContext } from 'react';
 import GenericDropdown from "../../components/buttons/GenericDropdown.jsx";
+import GameItem_EX from '../../components/experimental/GameItem_EX';
+
+
 
 const AllGamesPage = () => {
 
 	const [allGames, setAllGames] = useState([]);
-	// useEffect(() => {
-	// fetch(`https://www.freetogame.com/api/games?platform=${platform}&category=${genre}&sort-by=${sort}`)
-	// 	fetch(`https://www.freetogame.com/api/games?platform=browser&category=mmorpg&sort-by=release-date`)
-	// 		.then((res) => res.json())
-	// 		.then((data) => setAllGames(data))
-	// }, [])
-	// console.log(allGames);
+
+	useEffect(() => {
+		// fetch(`https://www.freetogame.com/api/games?platform=${platform}&category=${genre}&sort-by=${sort}`)
+		// fetch('https://www.freetogame.com/api/games?platform=browser&category=mmorpg&sort-by=release-date')
+		fetch('https://www.freetogame.com/api/games')
+			.then((res) => res.json())
+			.then((data) => setAllGames(data))
+	}, [])
+	console.log(allGames);
 
 
+	// Suche nach Games in SearchBar
 	const [searchText] = useContext(SearchTextContext)
 	console.log(searchText);
 
 
 	return (
 		<main className={style.allGamesPage}>
-			<GenericDropdown></GenericDropdown>
+			{/* <GenericDropdown></GenericDropdown> */}
+			<section className={style.gridList}>
+				{allGames.map((elt) => {
+					if (elt.title.toLowerCase().includes(searchText.toLowerCase())) {
+						return (
+							<GameItem_EX
+								key={elt.id}
+								img={elt.thumbnail}
+								title={elt.title}
+								platform={elt.platform}
+								genre={elt.genre}
+							/>
+						)
+					}
+				})}
+			</section>
 		</main>
 	);
 };
@@ -42,6 +63,19 @@ export default AllGamesPage;
 // 	{ value: 'Web Browser', label: 'Browser (Web)' },
 // ]
 // const [selectedOptionsPlatform, setSelectedOptionsPlatform] = useState([])
+
+// _______________
+// const MultiValue = ({ data, removeValue }) => {
+// 	return (
+// 		<div className="multi-value">
+// 			{data.label}
+// 			<span className="multi-value-remove" onClick={() => removeValue(data)}>
+// 				&times;
+// 			</span>
+// 		</div>
+// 	);
+// };
+
 // ___________________________
 // im return:
 {/* <Select
@@ -49,6 +83,7 @@ export default AllGamesPage;
 				options={optionsPlatform}
 				value={selectedOptionsPlatform}
 				onChange={(selected) => setSelectedOptionsPlatform(selected)}
+				components={{ MultiValue }}
 			/> */}
 
 // ============================================	
