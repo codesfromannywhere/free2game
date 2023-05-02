2899d3adeff585e1386842f87cd8cdf7d00da2cd
Justin Schildt <Just.schildt@gmail.com>
2023-05-03 01:46:39 +0200

    Added Click away listeners and quality of life changes

--- src/components/buttons/GenericDropdown.jsx
+++ src/components/buttons/GenericDropdown.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import style from './GenericDropdown.module.scss';
+import {ClickAwayListener} from "@mui/base";
 
 const GenericDropdown = ({
 	children,
@@ -14,48 +15,50 @@ const GenericDropdown = ({
 	const [isExpanded, setIsExpanded] = useState(false);
 	const [genreSearch, setGenreSearch] = useState("");
 	return (
-		<div className={[style.genericDropdown, className].join(" ")}{...props}>
-			<div className={style.selectBox}
-				style={{
-					position: "relative"
-				}}
-				onClick={() => {
-					setIsExpanded(!isExpanded);
-				}}>
-				<select>
-					<option>{title}</option>
-				</select>
-				<div className="overSelect" style={{
-					position: "absolute",
-					left: 0,
-					right: 0,
-					top: 0,
-					bottom: 0,
-				}}></div>
+		<ClickAwayListener onClickAway={() => {if(isExpanded) setIsExpanded(false)}}>
+			<div className={[style.genericDropdown, className].join(" ")}{...props}>
+				<div className={style.selectBox}
+					 style={{
+						 position: "relative"
+					 }}
+					 onClick={() => {
+						 setIsExpanded(!isExpanded);
+					 }}>
+					<select>
+						<option>{title}</option>
+					</select>
+					<div className="overSelect" style={{
+						position: "absolute",
+						left: 0,
+						right: 0,
+						top: 0,
+						bottom: 0,
+					}}></div>
+				</div>
+				<div style={{ display: isExpanded ? "block" : "none" }} className={style.options}>
+					<input type="text" id={"genre-search"} onChange={(event) => {
+						setGenreSearch(event.target.value);
+					}}/>
+					{options.filter(item => item.text.includes(genreSearch)).map((item, index) => {
+						return (
+							<label htmlFor={item.text} key={item.text + index}>
+								<input
+									type={singleOptionOnly ? "radio" : "checkbox"}
+									name={title}
+									id={item.text}
+									checked={item.state}
+									onChange={(event) => {
+										onOptionChange(index, event.target.checked, optionType);
+										setIsExpanded(false); //TODO: Change this to a Click Away listener because having to reopen the menu everytime you click an option is annoying.
+									}}
+								/>
+								{item.text}
+							</label>
+						);
+					})}
+				</div>
 			</div>
-			<div style={{ display: isExpanded ? "block" : "none" }} className={style.options}>
-				<input type="text" id={"genre-search"} onChange={(event) => {
-					setGenreSearch(event.target.value);
-				}}/>
-				{options.filter(item => item.text.includes(genreSearch)).map((item, index) => {
-					return (
-						<label htmlFor={item.text} key={item.text + index}>
-							<input
-								type={singleOptionOnly ? "radio" : "checkbox"}
-								name={title}
-								id={item.text}
-								checked={item.state}
-								onChange={(event) => {
-									onOptionChange(index, event.target.checked, optionType);
-									setIsExpanded(false); //TODO: Change this to a Click Away listener because having to reopen the menu everytime you click an option is annoying.
-								}}
-							/>
-							{item.text}
-						</label>
-					);
-				})}
-			</div>
-		</div>
+		</ClickAwayListener>
 	);
 };
 
--- src/components/buttons/GenericLink.jsx
+++ src/components/buttons/GenericLink.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './GenericLink.module.scss'
 import {Link} from "react-router-dom";
 
-const GenericLink = ({to, className, children, style}) => {
+const GenericLink = ({to, className, children, style, onClick, ...props}) => {
 	return (
-		<Link className={[className, styles.genericLink].join(" ")} to={to} style={style}>
+		<Link className={[className, styles.genericLink].join(" ")} to={to} style={style} onClick={onClick} {...props}>
 			{children}
 		</Link>
 	);
