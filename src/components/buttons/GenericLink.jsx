3961ea413dd2ea27c0f69e8efcf793e7b9ba4d6b
Justin Schildt <Just.schildt@gmail.com>
2023-05-02 11:47:09 +0200

    Fixed Show More color not being red

--- src/components/buttons/GenericLink.jsx
+++ src/components/buttons/GenericLink.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 const GenericLink = ({to, className, children, style}) => {
 	return (
-		<Link className={[styles.genericLink, className].join(" ")} to={to} style={style}>
+		<Link className={[className, styles.genericLink].join(" ")} to={to} style={style}>
 			{children}
 		</Link>
 	);
