6247b7f871d07df9e2e6d24f2bb5563f83807f00
codesfromannywhere <codesfromannywhere@gmail.com>
2023-05-02 16:46:57 +0200

    changed dropdown arrow

--- src/components/buttons/GenericDropdown.jsx
+++ src/components/buttons/GenericDropdown.jsx
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import style from './GenericDropdown.module.scss';
 
 const GenericDropdown = ({
 	children,
-	title="title",
+	title = "title",
 	options = [
 
 	],
 	className,
 	...props
-	 }) => {
+}) => {
 	const [isExpanded, setIsExpanded] = useState(false);
 	return (
 		<div className={[style.genericDropdown, className].join(" ")}{...props}>
 			<div className={style.selectBox}
-				 style={{
-					 position: "relative"
-				 }}
-				 onClick={() => {
-					 setIsExpanded(!isExpanded);
-				 }}>
+				style={{
+					position: "relative"
+				}}
+				onClick={() => {
+					setIsExpanded(!isExpanded);
+				}}>
 				<select>
 					<option>{title}</option>
 				</select>
@@ -31,15 +31,14 @@ const GenericDropdown = ({
 					bottom: 0,
 				}}></div>
 			</div>
-			<div style={{display: isExpanded ? "block" : "none"}} className={style.option}>
+			<div style={{ display: isExpanded ? "block" : "none" }} className={style.option}>
 				{options.map((item, index) => {
 					return (
 						<label htmlFor={item.text} key={item.text + index}>
 							<input
 								type="checkbox"
 								id={item.text}
-								onChange={(event) =>
-								{item.setChecked(event.target.value)}}
+								onChange={(event) => { item.setChecked(event.target.value) }}
 							/>
 							{item.text}
 						</label>
