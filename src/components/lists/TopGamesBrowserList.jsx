bf4c1e476f74daab649a839ca9744d706bbb3e8f
Justin Schildt <Just.schildt@gmail.com>
2023-05-04 12:51:41 +0200

    Added current month to top games

--- src/components/lists/TopGamesBrowserList.jsx
+++ src/components/lists/TopGamesBrowserList.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import GameItem from '../ListItems/GameItem.jsx'
 import style from "./HomeRecentList.module.scss";
+import {months} from "../../data/timeDate.js";
 
 
 const TopGamesBrowserList = () => {
@@ -8,7 +9,7 @@ const TopGamesBrowserList = () => {
     const [recentListData, setRecentListData] = useState([])
 
     useEffect(() => {
-        fetch(`https://www.freetogame.com/api/games?platform=browser&sort-by=release-date`)
+        fetch(`https://www.freetogame.com/api/games?platform=browser&sort-by=popularity`)
             .then(res => res.json())
             .then(data => {
                 // console.log(data.slice(0, 4))
@@ -17,7 +18,7 @@ const TopGamesBrowserList = () => {
     }, [])
     return (
         <section className={style.gameCardSec}>
-            <h2>Top 4 Games for Browser in June 2022</h2>
+            <h2>Top 4 Games for Browser in {months[new Date().getMonth()]} {new Date().getFullYear()}</h2>
             <article className={style.gameCardContainer}>
                 {recentListData.map((game, i) => {
                     // console.log(game.title)
