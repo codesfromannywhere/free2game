7b5df4516dd659b34a71304a5b6e9b21f770d5f9
Jenseko <jenscode23@gmail.com>
2023-05-04 14:17:25 +0200

    center mainsection with margin?

--- src/pages/GameDetailsPage/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage/GameDetailsPage.jsx
@@ -67,67 +67,69 @@ const GameDetailsPage = () => {
 					<HeroSection backgroundImage={gameDetails.screenshots[0]?.image} />
 				</section>
 			)}
-			<h2>{gameDetails.title.toUpperCase()}</h2>
-			<article className={style.article}>
-				<div className={style.thumbnail_button_article}>
-					<img src={gameDetails.thumbnail} alt={`thumbnail ${gameDetails.title}`} />
-					<h3>Platform: {gameDetails.platform}</h3>
-					<FilterTag className={style.Game_tag} text={gameDetails.genre} />
-					<GenericLink className={style.Link_playnow} to={gameDetails.freetogame_profile_url}>PLAY NOW</GenericLink>
-				</div>
-				<div className={style.about_text_div}>
-					<h3>About</h3>
-					<p>{gameDetails.description}</p>
-				</div>
-			</article>
-			{gameDetails.screenshots[0]?.image && (
-				<section className={style.screenshot_section}>
-					<img src={gameDetails.screenshots[1]?.image} alt="Screenshot 2" />
-					<img src={gameDetails.screenshots[2]?.image} alt="Screenshot 3" />
-				</section>
-			)}
-			<article className={style.main_article_system}>
-				<article className={style.information}>
-					<h3 className={style.headline}>Additional Information</h3>
-					<p>Please note this free-to-play game may or may not offer optional in-game purchases.</p>
-					<div className={style.developer_block}>
-						<div>
-							<h3>Developer</h3>
-							<h3>Publisher</h3>
-							<h3>Release Date</h3>
-						</div>
-						<div>
-							<p>{gameDetails.developer}</p>
-							<p>{gameDetails.publisher}</p>
-							<p>{fixedReleaseDate}</p>
-						</div>
+			<section className={style.main_section}>
+				<h2>{gameDetails.title.toUpperCase()}</h2>
+				<article className={style.article}>
+					<div className={style.thumbnail_button_article}>
+						<img src={gameDetails.thumbnail} alt={`thumbnail ${gameDetails.title}`} />
+						<h3>Platform: {gameDetails.platform}</h3>
+						<FilterTag className={style.Game_tag} text={gameDetails.genre} />
+						<GenericLink className={style.Link_playnow} to={gameDetails.freetogame_profile_url}>PLAY NOW</GenericLink>
+					</div>
+					<div className={style.about_text_div}>
+						<h3>About</h3>
+						<p>{gameDetails.description}</p>
 					</div>
 				</article>
-				{gameDetails.minimum_system_requirements?.os && gameDetails.minimum_system_requirements?.processor && (
-					<article className={style.requirements_article}>
-						<h3 className={style.headline}>Minimum System Requirements</h3>
-						<div className={style.system_block_div}>
-							<div className={style.os_memory_storage}>
-								<h3>OS</h3>
-								<p>{gameDetails.minimum_system_requirements?.os}</p>
-								<h3>Memory</h3>
-								<p>{gameDetails.minimum_system_requirements?.memory}</p>
-								<h3>Storage</h3>
-								<p>{gameDetails.minimum_system_requirements?.storage}</p>
+				{gameDetails.screenshots[0]?.image && (
+					<section className={style.screenshot_section}>
+						<img src={gameDetails.screenshots[1]?.image} alt="Screenshot 2" />
+						<img src={gameDetails.screenshots[2]?.image} alt="Screenshot 3" />
+					</section>
+				)}
+				<article className={style.main_article_system}>
+					<article className={style.information}>
+						<h3 className={style.headline}>Additional Information</h3>
+						<p>Please note this free-to-play game may or may not offer optional in-game purchases.</p>
+						<div className={style.developer_block}>
+							<div>
+								<h3>Developer</h3>
+								<h3>Publisher</h3>
+								<h3>Release Date</h3>
 							</div>
-							<div className={style.processor_graphics_notes}>
-								<h3>Processor</h3>
-								<p>{gameDetails.minimum_system_requirements?.processor}</p>
-								<h3>Graphics</h3>
-								<p>{gameDetails.minimum_system_requirements?.graphics}</p>
-								<h3>Additional Notes</h3>
-								<p>Specifications may change during development</p>
+							<div>
+								<p>{gameDetails.developer}</p>
+								<p>{gameDetails.publisher}</p>
+								<p>{fixedReleaseDate}</p>
 							</div>
 						</div>
 					</article>
-				)}
-			</article>
-		</main>
+					{gameDetails.minimum_system_requirements?.os && gameDetails.minimum_system_requirements?.processor && (
+						<article className={style.requirements_article}>
+							<h3 className={style.headline}>Minimum System Requirements</h3>
+							<div className={style.system_block_div}>
+								<div className={style.os_memory_storage}>
+									<h3>OS</h3>
+									<p>{gameDetails.minimum_system_requirements?.os}</p>
+									<h3>Memory</h3>
+									<p>{gameDetails.minimum_system_requirements?.memory}</p>
+									<h3>Storage</h3>
+									<p>{gameDetails.minimum_system_requirements?.storage}</p>
+								</div>
+								<div className={style.processor_graphics_notes}>
+									<h3>Processor</h3>
+									<p>{gameDetails.minimum_system_requirements?.processor}</p>
+									<h3>Graphics</h3>
+									<p>{gameDetails.minimum_system_requirements?.graphics}</p>
+									<h3>Additional Notes</h3>
+									<p>Specifications may change during development</p>
+								</div>
+							</div>
+						</article>
+					)}
+				</article>
+			</section>
+		</main >
 	);
 };
 
